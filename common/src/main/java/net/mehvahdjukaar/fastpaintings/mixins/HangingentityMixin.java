23a128515f149c3906f7ed21da00442cc00bbb80
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-08-26 17:10:53 +0200

    license

--- common/src/main/java/net/mehvahdjukaar/fastpaintings/mixins/HangingentityMixin.java
+++ common/src/main/java/net/mehvahdjukaar/fastpaintings/mixins/HangingentityMixin.java
@@ -3,6 +3,7 @@ package net.mehvahdjukaar.fastpaintings.mixins;
 import net.mehvahdjukaar.fastpaintings.PaintingBlock;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.decoration.BlockAttachedEntity;
 import net.minecraft.world.entity.decoration.HangingEntity;
 import net.minecraft.world.entity.decoration.Painting;
 import net.minecraft.world.level.Level;
@@ -12,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(HangingEntity.class)
+@Mixin(BlockAttachedEntity.class)
 public abstract class HangingentityMixin extends Entity {
 
     protected HangingentityMixin(EntityType<?> entityType, Level level) {
